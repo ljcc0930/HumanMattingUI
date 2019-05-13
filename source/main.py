63931cabab3ccacaea2d868a3aa2b5cd9b6ed3b3
ljcc0930 <ljcc0930@qq.com>
2019-05-13 17:44:20 +0800

    nothing

--- source/main.py
+++ source/main.py
@@ -13,7 +13,9 @@ def main():
         c = lambda x, y : closed_form_matting_with_trimap(x / 255.0, y[:, :, 0] / 255.0) * 255.0
         loadList = '../final_list.txt'
         methods = [a, b, c]
+        print("load model success!!!")
     except:
+        print("load model failed...")
         a = lambda x, y: y
         b = lambda x, y: x
         c = lambda x, y: x / 2 + y / 2
